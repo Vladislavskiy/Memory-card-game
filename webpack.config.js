ed7fa198f666588b2931b7f4c3bb7cbadcb85f69
Владислав Соловьев <vladislavsolovev@Vladislavs-MacBook-Pro.local>
2023-07-25 01:52:16 +0300

    added webpack, htmlWebpackPlugin, watch mode

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index_bundle.js',
+    clean: true,
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+    }),
+  ],
+};
